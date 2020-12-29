27b3d15d55fa3819f7819082fdb4b20ec42f08ce
Rojber <rm5672@gmail.com>
2020-12-29 16:01:34 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,105 @@
+from collections import OrderedDict
+import tree
+
+
+def encrypt_data(filename, encoded_filename):
+    char_probability = {}
+    char_counter = 0
+
+    text = open(filename, encoding="utf-8").read()
+
+    for char in text:
+        char_counter += 1
+        if char == "\n":
+            char = "_NewLine"
+        if char == " ":
+            char = "_Space"
+        if char in char_probability:
+            char_probability[char] += 1
+        else:
+            char_probability[char] = 1
+
+    char_probability = OrderedDict(sorted(char_probability.items()))
+
+    for char in char_probability:
+        char_probability[char] = float(char_probability[char]/char_counter)
+        print(char + " = " + str(char_probability[char]))
+
+    sorted_probability_dict = {k: v for k, v in sorted(char_probability.items(), key=lambda x: x[1], reverse=True)}
+    print(sorted_probability_dict)
+    root = tree.Tree("root")
+    compute(root, [], sorted_probability_dict)
+    root.print_tree()
+
+    codes_dict = {}
+    codes_dict = root.get_codes_dict(codes_dict, [])
+    print(codes_dict)
+
+    bitstring = ''
+    for character in text:
+        if character == "\n":
+            character = "_NewLine"
+        if character == " ":
+            character = "_Space"
+        bitstring += codes_dict[character]
+
+    with open(encoded_filename, 'bw') as f:
+        f.write(bytes(int(bitstring[i : i + 8], 2) for i in range(0, len(bitstring), 8)))
+
+
+def compute(code_tree, path, sorted_probability_dict):
+    print()
+    print(sorted_probability_dict)
+    print(path)
+    last_sum = 0.0
+    string_sum = 0.0
+    substring = {}
+    temp_string = sorted_probability_dict.copy()
+
+    if len(sorted_probability_dict) == 1:
+        return
+
+    if len(sorted_probability_dict) == 2:
+        dict_keys = list(sorted_probability_dict.keys())
+        code_tree.add_to_tree(path + [0], dict_keys[0])
+        code_tree.add_to_tree(path + [1], dict_keys[1])
+        return
+
+    for _, value in sorted_probability_dict.items():
+        string_sum += value
+
+    for key, value in sorted_probability_dict.items():
+        if last_sum == 0.0:
+            last_sum = value
+            substring[key] = value
+            temp_string.pop(key)
+        else:
+            actual_sum = last_sum + value
+            print()
+            print("String sum: " + str(string_sum/2))
+            print("Actual sum:" + str(actual_sum))
+            print("Last sum: " + str(last_sum))
+            print()
+            if ((string_sum/2) - actual_sum < 0 and (string_sum/2) - last_sum >= 0) or ((string_sum/2) - actual_sum <= 0 and (string_sum/2) - last_sum <= 0):
+                if abs(actual_sum - (string_sum/2)) >= abs(last_sum - (string_sum/2)):
+                    code_tree.add_to_tree(path + [0], ''.join(list(substring.keys())))
+                    code_tree.add_to_tree(path + [1], ''.join(list(temp_string.keys())))
+                    compute(code_tree, path + [0], substring)
+                    compute(code_tree, path + [1], temp_string)
+                    return
+                else:
+                    substring[key] = value
+                    temp_string.pop(key)
+                    code_tree.add_to_tree(path + [0], ''.join(list(substring.keys())))
+                    code_tree.add_to_tree(path + [1], ''.join(list(temp_string.keys())))
+                    compute(code_tree, path + [0], substring)
+                    compute(code_tree, path + [1], temp_string)
+                    return
+            else:
+                last_sum = actual_sum
+                substring[key] = value
+                temp_string.pop(key)
+
+
+if __name__ == '__main__':
+    encrypt_data("test.txt", "test_packed.pacc")
---
+++ tree.py
@@ -0,0 +1,40 @@
+class Tree:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def print_tree(self):
+        print(self.data)
+        if self.left is not None:
+            print('LEFT')
+            self.left.print_tree()
+        if self.right is not None:
+            print('RIGHT')
+            self.right.print_tree()
+
+    def get_codes_dict(self, codes_dict, path):
+        if self.left is not None:
+            if self.left.data != '_NewLine' and self.left.data != '_Space' and len(self.left.data) > 1:
+                self.left.get_codes_dict(codes_dict, path + [0])
+            else:
+                codes_dict[self.left.data] = ''.join(str(x) for x in path + [0])
+        if self.right is not None:
+            if self.right.data != '_NewLine' and self.right.data != '_Space' and len(self.right.data) > 1:
+                self.right.get_codes_dict(codes_dict, path + [1])
+            else:
+                codes_dict[self.right.data] = ''.join(str(x) for x in path + [1])
+        return codes_dict
+
+    def add_to_tree(self, path, data):
+        if len(path) == 1:
+            if path[0] == 0:
+                self.left = Tree(data)
+            else:
+                self.right = Tree(data)
+        elif path[0] == 0:
+            path.pop(0)
+            self.left.add_to_tree(path, data)
+        else:
+            path.pop(0)
+            self.right.add_to_tree(path, data)
