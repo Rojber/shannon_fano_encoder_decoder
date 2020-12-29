dafdd487b95ead4b8beb5e62b6312a28e3bfde09
Rojber <rm5672@gmail.com>
2020-12-29 19:00:42 +0100

    Interface update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import json
 from collections import OrderedDict
 import tree
+import time
 
 
 def encrypt_data(filename, encoded_filename):
     char_probability = {}
     char_counter = 0
+    start = time.time()
 
     text = open(filename, encoding="utf-8").read()
 
@@ -23,13 +26,13 @@ def encrypt_data(filename, encoded_filename):
 
     for char in char_probability:
         char_probability[char] = float(char_probability[char]/char_counter)
-        print(char + " = " + str(char_probability[char]))
+        # print(char + " = " + str(char_probability[char]))
 
     sorted_probability_dict = {k: v for k, v in sorted(char_probability.items(), key=lambda x: x[1], reverse=True)}
-    print(sorted_probability_dict)
+    # print(sorted_probability_dict)
     root = tree.Tree("root")
     compute(root, [], sorted_probability_dict)
-    root.print_tree()
+    # root.print_tree()
 
     codes_dict = {}
     codes_dict = root.get_codes_dict(codes_dict, [])
@@ -44,13 +47,18 @@ def encrypt_data(filename, encoded_filename):
         bitstring += codes_dict[character]
 
     with open(encoded_filename, 'bw') as f:
+        f.write(bytes(json.dumps(codes_dict, ensure_ascii=False).encode('utf8')))
+        f.write((0).to_bytes(8, 'big'))
         f.write(bytes(int(bitstring[i : i + 8], 2) for i in range(0, len(bitstring), 8)))
+
+    end = time.time()
+    print("Encoding finished in " + str(end - start) + " seconds.")
 
 
 def compute(code_tree, path, sorted_probability_dict):
-    print()
-    print(sorted_probability_dict)
-    print(path)
+    # print()
+    # print(sorted_probability_dict)
+    # print(path)
     last_sum = 0.0
     string_sum = 0.0
     substring = {}
@@ -75,11 +83,11 @@ def compute(code_tree, path, sorted_probability_dict):
             temp_string.pop(key)
         else:
             actual_sum = last_sum + value
-            print()
-            print("String sum: " + str(string_sum/2))
-            print("Actual sum:" + str(actual_sum))
-            print("Last sum: " + str(last_sum))
-            print()
+            # print()
+            # print("String sum: " + str(string_sum/2))
+            # print("Actual sum:" + str(actual_sum))
+            # print("Last sum: " + str(last_sum))
+            # print()
             if ((string_sum/2) - actual_sum < 0 and (string_sum/2) - last_sum >= 0) or ((string_sum/2) - actual_sum <= 0 and (string_sum/2) - last_sum <= 0):
                 if abs(actual_sum - (string_sum/2)) >= abs(last_sum - (string_sum/2)):
                     code_tree.add_to_tree(path + [0], ''.join(list(substring.keys())))
@@ -102,4 +110,20 @@ def compute(code_tree, path, sorted_probability_dict):
 
 
 if __name__ == '__main__':
-    encrypt_data("test.txt", "test_packed.pacc")
+    while True:
+        print("Shannon-Fano encryption and decryption utility.")
+        # print("Do you want encrypt or decrypt file? (e/d)")
+        inp1 = input("Do you want encrypt or decrypt file? (e/d): ")
+        if inp1 == 'e':
+            inp_e1 = input("Relative path to file: ")
+            inp_e2 = input("Encoded file name: ")
+            encrypt_data(inp_e1, inp_e2 + ".pack")
+        elif inp1 == 'd':
+            # TUTAJ DODAJ WYWOŁANIE FUNKCJI OD DEKODOWANIA
+            pass
+        else:
+            print('WRONG COMMAND\n')
+            continue
+        inp2 = input("\nDo you want to continue with another file? (y/n): ")
+        if inp2 == 'n':
+            break
